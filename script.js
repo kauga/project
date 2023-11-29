dde766244cfef99a7a1449ee0eb561ef32071260
kauga <alexlucas1944@gmail.com>
2023-11-29 02:54:48 -0800

    new css

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
-const btnEl = document.querySelector('.menuMenu');
-const btnHeadEl = document.querySelector('.mainMenu');
+// const btnEl = document.querySelector('.menuMenu');
+// const btnHeadEl = document.querySelector('.mainMenu');
 
-btnEl.addEventListener('click', function() {
-  btnEl.classList.toggle('active');
-  // btnHeadEl.classList.toggle('mainMenu');
-  console.log(`kauga huyu`);
-})
+// btnEl.addEventListener('click', function() {
+//   btnEl.classList.toggle('active');
+//   btnHeadEl.classList.toggle('mainMenu');
+//   console.log(`kauga huyu`);
+// })
+
+const btnEl = document.querySelector('.mainMenu');
+const btnOpen = document.querySelector('.menuOpen');
+const btnClose = document.querySelector('.menuClose');
+
+btnOpen.addEventListener('click', show);
+btnClose.addEventListener('click', close);
+
+function show() {
+  btnEl.style.display = 'flex';
+  btnEl.style.top = '0';
+  console.log(`hellow`);
+}
+
+function close() {
+  btnEl.style.top = '-100%';
+  console.log(`hellow world`);
+}
